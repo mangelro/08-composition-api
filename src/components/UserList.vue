c13c453decbf16438246cab7d6673dbadb4ac170
Miguel A. Romera <miguelromera@gmail.com>
2022-05-09 18:25:48 +0200

    Slot scoped

---
+++ src/components/UserList.vue
@@ -0,0 +1,33 @@
+<template>
+	<ul>
+		<li>
+			<slot v-for="user in users" 
+			:user=user
+			:key="user.id"></slot>
+			<!-- <h4>{{ first_name }} {{ last_name }}</h4>
+			<h6>{{ email }}</h6> -->
+		</li>
+	</ul>
+</template>
+
+<script>
+	export default {
+		props:{
+			users:{
+				type: Array,
+				required: true
+			}
+		}
+
+
+
+	}
+</script>
+
+<style scoped>
+
+ul{
+	list-style: none;
+}
+
+</style>
